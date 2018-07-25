99060dfcaada033f4d74d6bac18736729e85b937
Peter <kubasov@gmail.com>
2018-07-25 23:37:51 +0300

    webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,16 @@ module.exports = {
 					prod ? MiniCssExtractPlugin.loader : 'style-loader',
 					'css-loader'
 				]
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts/'
+					}
+				}]
 			}
 		]
 	},
