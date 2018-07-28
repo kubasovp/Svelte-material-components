a10bb1f0f7d8fe2244431910a8499864facb2494
Peter <kubasov@gmail.com>
2018-07-28 17:13:19 +0300

    Полезный комментарий к коммиту

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import App from './App.html';
 import 'material-design-icons/iconfont/material-icons.css';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/js/waves.js';
 
 const app = new App({
 	target: document.body,
